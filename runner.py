3cea074c5c28f008c9c65b0f2977d4082efe6e38
Akash Pamal <akashpamal@gmail.com>
2021-05-29 21:50:54 -0400

    added multiprocessing capabilities

--- runner.py
+++ runner.py
@@ -6,39 +6,20 @@ import copy
 import numpy as np
 from pygame.locals import KEYDOWN, KEYUP, MOUSEBUTTONDOWN, MOUSEBUTTONUP, QUIT
 from PIL import Image
+import json
+import multiprocessing
 
 from kmeansIdentifier import kMeans_quantization
 from saving import Saver
 
-# AstroArt
-
-pg.init()
-clock = pg.time.Clock()
-pg.display.set_caption("Astro-Art Animator")
-
-running = True
 FPS = 120
-WINDOW_TOLERANCE = 50
-# WINDOW_SIZE = (500, 500)
+WINDOW_TOLERANCE = 25
 WINDOW_SIZE = (1440, 800)
-screen = pg.display.set_mode(WINDOW_SIZE)
-screen_rect = screen.get_rect()
-display = pg.Surface(WINDOW_SIZE)
-display_rect = display.get_rect()
 
 G = 6.67408 * (10 ** -11)  # Gravitational Constant
 MASS_AREA_RATIO = 2 * (10 ** 9)  # mass in kilograms to area in pixels
+NUM_PLANETS = 60
 
-planet_list = []
-planet_id = 0
-
-NUM_PLANETS = 5
-IMAGE = "./input_images/DABABY CAR LESSS GOOOOOO.jpeg"
-img = Image.open(IMAGE)
-# colors = kMeans_quantization(img, NUM_PLANETS)
-#colors = ((100, 100, 0), (255, 0, 255), (100, 255, 255), (0, 255, 0))
-# colors = [(243, 243, 244), (197, 89, 32), (148, 152, 164), (63, 27, 17), (209, 109, 47), (159, 61, 30), (58, 66, 82), (224, 161, 113), (39, 48, 62), (209, 179, 168), (165, 83, 56), (135, 46, 20), (87, 94, 109), (25, 26, 35), (137, 63, 47), (145, 91, 79), (215, 214, 219), (212, 123, 72), (48, 32, 37), (105, 58, 50), (7, 7, 15), (114, 121, 137), (179, 72, 32), (216, 138, 93), (188, 93, 51), (176, 183, 197), (79, 45, 41), (172, 125, 112), (103, 36, 17), (195, 107, 69)]
-colors = [(random.random() * 255, random.random() * 255, random.random() * 255) for i in range(5)]
 
 class Planet:
     def __init__(self, vel_x, vel_y, x, y, radius, id, color=[255, 0, 0]):
@@ -54,7 +35,6 @@ class Planet:
         )
         self.mass = math.pi * (self.radius ** 2) * MASS_AREA_RATIO
         self.id = id
-        # self.last_pos = (x, y)
         self.velocity = [vel_x, vel_y]
         self.color = color
         
@@ -101,48 +81,86 @@ class Planet:
     def draw(self):
         pg.draw.circle(screen, self.color, (int(self.x), int(self.y)), int(self.radius))
         
-    
-for num, item in enumerate(colors):
-    planet_list.append(Planet(random.random()-0.5, random.random()-0.5, random.random() * WINDOW_SIZE[0], random.random() * WINDOW_SIZE[1], random.random() * 5, num, color=item))
-    
-planet_list2 = copy.deepcopy(planet_list)
-    
-frame_num = 0
-while running:
-    # screen.fill([0, 0, 0])
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_q:
-                running = False
-    
-    
-    for planet in planet_list:
-        if planet.x > WINDOW_SIZE[0] + WINDOW_TOLERANCE or planet.x < -WINDOW_TOLERANCE:
-            planet_list.remove(planet)
-            print(len(planet_list))
-        if planet.y > WINDOW_SIZE[1] + WINDOW_TOLERANCE or planet.y < -WINDOW_TOLERANCE:
-            planet_list.remove(planet)
-            print(len(planet_list))
-        if planet.velocity[0] > 100 or planet.velocity[0] < -100:
-            planet_list.remove(planet)
-            print(len(planet_list))
-        if planet.velocity[1] > 100 or planet.velocity[1] < -100:
-            planet_list.remove(planet)
-            print(len(planet_list))
+
+def run_simulation(number):
+    pg.init()
+    CLOCK = pg.time.Clock()
+    pg.display.set_caption("Astro-Art Animator")
+    kmeans = False
+    image = None
+    if kmeans:
+        # IMAGE = "./input_images/DABABY CAR LESSS GOOOOOO.jpeg"
+        img = Image.open(image)
+        colors = kMeans_quantization(img, NUM_PLANETS)
+    else:
+        colors = [(random.random() * 255, random.random() * 255, random.random() * 255) for i in range(NUM_PLANETS)]
+
+    # with open('./training_data/69696420.json', 'r') as data:
+    #     init_conds = json.load(data)
+
+    # for num, planet in enumerate(init_conds.values()):
+    #     planet_list.append(Planet(vel_x = planet[0], vel_y = planet[1], x = planet[2], y = planet[3], radius = planet[4], id = num, color = planet[5]))
+
+    global screen, display, num_planets, planet_list
+    screen = pg.display.set_mode(WINDOW_SIZE)
+    screen_rect = screen.get_rect()
+    display = pg.Surface(WINDOW_SIZE)
+    display_rect = display.get_rect()
+
+    planet_list = []
+
+
+    for num, item in enumerate(colors):
+        planet_list.append(Planet(random.random()-0.5, random.random()-0.5, random.random() * WINDOW_SIZE[0], random.random() * WINDOW_SIZE[1], random.random() * 5, num, color=item))
+        
+    saver = Saver(number, screen, copy.deepcopy(planet_list))
+        
+    running = True
+    while running:
+        # screen.fill([0, 0, 0])
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_q:
+                    running = False
+        
+        for planet in planet_list:
+            if planet.x > WINDOW_SIZE[0] + WINDOW_TOLERANCE or planet.x < -WINDOW_TOLERANCE:
+                planet_list.remove(planet)
+                print(len(planet_list))
+            elif planet.y > WINDOW_SIZE[1] + WINDOW_TOLERANCE or planet.y < -WINDOW_TOLERANCE:
+                planet_list.remove(planet)
+                print(len(planet_list))
+            elif planet.velocity[0] > 100 or planet.velocity[0] < -100:
+                planet_list.remove(planet)
+                print(len(planet_list))
+            elif planet.velocity[1] > 100 or planet.velocity[1] < -100:
+                planet_list.remove(planet)
+                print(len(planet_list))
+            # elif (planet.velocity[0] ** 2 + planet.velocity[1] ** 2) ** 0.5 < 0.001:
+            #     planet_list.remove(planet)
+            #     print(len(planet_list))
+                
+        if len(planet_list) == 0:
+            saver.save_data()
+            quit()
             
-    if len(planet_list) == 0:
+        for planet in planet_list:
+            planet.update()
         for planet in planet_list:
             planet.draw()
-        Saver(100, screen, planet_list2)
-         
-    for planet in planet_list:
-        planet.update()
-    for planet in planet_list:
-        planet.draw()
 
-    pg.display.flip()
-    # print('Flipped screen')
-    clock.tick(FPS)
-    frame_num += 1
-    # if end_simulation():
-    #     pg.display.flip()
+        pg.display.flip()
+        CLOCK.tick(FPS)
+
+
+if __name__ == '__main__':
+    NUM_PROCESSES = None
+    save_file_numbers = [(elem, ) for elem in range(100)]
+    if NUM_PROCESSES is None:
+        with multiprocessing.Pool() as pool:
+            pool.starmap(run_simulation, save_file_numbers)
+    else:
+        with multiprocessing.Pool(NUM_PROCESSES) as pool:
+            pool.starmap(run_simulation, save_file_numbers)
+    # run_simulation(1)
+    # run_simulation(2)
--- saving.py
+++ saving.py
@@ -6,15 +6,13 @@ class Saver:
         self.number = number
         self.screen = screen
         self.planets = planets
-        
-        self.save_data()
     
     def save_data(self):
         pg.image.save(self.screen, "./training_data/" + str(self.number) + ".png")
         
         modified_planets = dict()
         for num, item in enumerate(self.planets):
-            modified_planets[num] = (item.x, item.y, item.velocity[0], item.velocity[1], item.radius, (item.color[0], item.color[1], item.color[2]))
+            modified_planets[num] = (item.velocity[0], item.velocity[1], item.x, item.y, item.radius, (item.color[0], item.color[1], item.color[2]))
         
         with open("./training_data/" + str(self.number) + ".json", 'w') as out:  
             json.dump(modified_planets, out)
